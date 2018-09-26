aeaa43689a5152b6750ff67bf84310d17006c4ae
brianstoiber <brian@brianstoiber.com>
2018-09-26 07:33:57 -0500

    update of test

--- src/test/IteratorPatternTest.java
+++ src/test/IteratorPatternTest.java
@@ -28,7 +28,7 @@ public class IteratorPatternTest {
 	private static CustomHashTableImpl customCookieHashTable;
 	private static ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	private static PrintStream originalOut = System.out;
-	
+
 	@BeforeAll
 	public static void setUp() {
 		printer = new MenuPrinter();
@@ -36,18 +36,18 @@ public class IteratorPatternTest {
 		customCookieHashTable = new CustomHashTableImpl();
 		// setting system output to a printstream
 		// used for capturing sysouts in the print menu
-	    System.setOut(new PrintStream(outContent));
+		System.setOut(new PrintStream(outContent));
 	}
-	
+
 	@AfterAll
 	public static void tearDown() {
 		printer = null;
 		customCookieHashTable = null;
 		customIcecreamArrayList = null;
-		//setting back system output to original one
-	    System.setOut(originalOut);
+		// setting back system output to original one
+		System.setOut(originalOut);
 	}
-	
+
 	@Test
 	public void testPrinter() {
 		// adding ice cream flavors to customArray
@@ -73,10 +73,11 @@ public class IteratorPatternTest {
 		customIcecreamArrayList.addFlavor(icecream);
 		icecream = new Icecream();
 		icecream.setName("RASPBERRY RIPPLE");
-		// I had this on a class trip my senior year of high school and have wish I could find it in the US
+		// I had this on a class trip my senior year of high school and have wish I
+		// could find it in the US
 		icecream.setDescription("Raspberry syrup injected into vanilla ice cream");
 		customIcecreamArrayList.addFlavor(icecream);
-		
+
 		// Adding cookie types to hashTable
 		Cookie c1 = new Cookie();
 		c1.setName("CHOCOLATE CHIP");
@@ -94,30 +95,30 @@ public class IteratorPatternTest {
 		c1.setName("FORTUNE COOKIE");
 		c1.setDescription("Crisp Sugar cookies with a hidden fortune (poorly written) inside");
 		customCookieHashTable.addCookie(c1);
-		
+
 		// calling printer.printMenu
 		printer.printMenu(customIcecreamArrayList, customCookieHashTable);
-		
-		// asserting the output with expected output stored in file
-		// using trim function to remove extra spaces if any and get only the string values
-		
+
 		/**
-		 * I tried to compare the output to the contents on sampleMenu.txt 
-		 * but the test keeps failing and I can't figure out the cause of it
+		 * I tried to compare the output to the contents on sampleMenu.txt but the test
+		 * keeps failing and I can't figure out the cause of it
 		 */
-		 assertEquals(outContent.toString().trim(),getFileContents("sampleMenu.txt").trim());
+		// asserting the output with expected output stored in file
+		// using trim function to remove extra spaces if any and get only the string
+		// values
+		assertEquals(outContent.toString().trim(), getFileContents("sampleMenu.txt").trim());
 	}
-	
+
 	// helper function that is used to get file contents.
 	private String getFileContents(String filePath) {
 		StringBuilder builder = new StringBuilder();
 		InputStream istream = this.getClass().getClassLoader().getResourceAsStream(filePath);
-		try(BufferedReader reader = new BufferedReader(new InputStreamReader(istream, Charset.defaultCharset()))){
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(istream, Charset.defaultCharset()))) {
 			builder.append(reader.lines().collect(Collectors.joining(System.lineSeparator())));
 		} catch (IOException e) {
 			System.out.println("Exception encountered while reading file..... " + e.getMessage());
 		}
 		return builder.toString();
 	}
-	
+
 }
